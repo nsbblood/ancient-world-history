65705a83bd8efd7301d4624b65b8bd1273c2e3d8
nsbblood <enesarikan4444@gmail.com>
2025-11-02 00:36:51 +0300

    Initial Commit

---
+++ Ancient World Stories/Ancient_World_StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ancient_World_StoriesApp.swift
+//  Ancient World Stories
+//
+//  Created by enes arikan on 2.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Ancient_World_StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ancient World StoriesTests/Ancient_World_StoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ancient_World_StoriesTests.swift
+//  Ancient World StoriesTests
+//
+//  Created by enes arikan on 2.11.2025.
+//
+
+import Testing
+@testable import Ancient_World_Stories
+
+struct Ancient_World_StoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
